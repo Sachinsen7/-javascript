fc5347c263b90536e680cd4099eec7e3cbc19ec6
SACH!N <143265500+Sachinsen7@users.noreply.github.com>
2024-04-05 02:49:49 +0000

    Conversions of datatypes

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,8 +1,35 @@
-let score = "33abc"
-
+let score = "Sachin"
 console.log(typeof score);
 console.log(typeof(score));
 
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN Not a Number 
+// true => 1; false => 0
+// undefined => undefined
+// null =>  object 
+// string => string
+
+let isLoggedin = "Sachin"
+
+let booleanIsLoggedin = Boolean(isLoggedin)
+console.log(booleanIsLoggedin);
+
+// 1 => true;  0 => false 
+// "" => false 
+// "Sachin" => true
+
+let someNumber = 12365
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
+
+// 3325 => string 
+// "Sachin" => string
+
+
+
