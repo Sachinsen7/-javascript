5e5ea2492c73fac4029375db943133a74fab5649
SACH!N <143265500+Sachinsen7@users.noreply.github.com>
2024-04-05 15:36:03 +0000

    operations done

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -32,4 +32,27 @@ console.log(typeof stringNumber);
 // "Sachin" => string
 
 
+// *************** Operations **************
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Sachin" /*given space because i wanted this */
+let str3 = str1 + str2 
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+
 
