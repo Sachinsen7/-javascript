96b3c96cc8ef5eec95c4b29b84eddd3ccaceda18
SACH!N <143265500+Sachinsen7@users.noreply.github.com>
2024-04-07 03:12:47 +0000

    Conversions operations done

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,10 +1,10 @@
 let score = "Sachin"
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN Not a Number 
@@ -16,7 +16,7 @@ console.log(valueInNumber);
 let isLoggedin = "Sachin"
 
 let booleanIsLoggedin = Boolean(isLoggedin)
-console.log(booleanIsLoggedin);
+// console.log(booleanIsLoggedin);
 
 // 1 => true;  0 => false 
 // "" => false 
@@ -25,8 +25,8 @@ console.log(booleanIsLoggedin);
 let someNumber = 12365
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
 
 // 3325 => string 
 // "Sachin" => string
@@ -35,7 +35,7 @@ console.log(typeof stringNumber);
 // *************** Operations **************
 let value = 3
 let negValue = -value
-console.log(negValue);
+// console.log(negValue);
 
 // console.log(2+2);
 // console.log(2-2);
@@ -46,13 +46,41 @@ console.log(negValue);
 let str1 = "Hello"
 let str2 = " Sachin" /*given space because i wanted this */
 let str3 = str1 + str2 
-console.log(str3);
+// console.log(str3);
 
-console.log("1" + 2);
-console.log(1 + "2");
-console.log("1" + 2 + 2);
-console.log(1 + 2 + "2");
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
 
+// console.log((3 * 4) + 5 % 6);
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+
+gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// let x = 5;
+// let y = ++x;
+// console.log(x); // Output: 6
+// console.log(y); // Output: 6
+
+let a = 3;
+let b = ++a;
+console.log(a);
+console.log(b);
+
+let c = 5;
+let d = c++;
+console.log(c); // Output: 6
+console.log(d); // Output: 6
 
 
 
