1cdcb4faf7923478a440ad9671ef0034f0e7aed9
SACH!N <143265500+Sachinsen7@users.noreply.github.com>
2024-04-10 03:10:59 +0000

    datatypes summary

--- 01_basics/05_datatypes-summary.js
+++ 01_basics/05_datatypes-summary.js
@@ -3,7 +3,41 @@
 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt */
 
 const score = 50
+const scoreValue = 50.3
+
+const isLoggedIn = false
+const outsideTemp = null 
+
+let userEmail;
+// let userEmail = undefined;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 454654158975 /* number */
+// const bigNumber = 454654158975n  result : bigint
 
 /* Reference (Non primitive)
 
 Array, Objects, Functions */
+
+
+const heros = ["Captain America", "Tony Stark", "Hulk"];  /* Array */
+
+let myObj = {
+    name: "Sachin",
+    age: 20,
+}
+
+const myFunction = function(){}
+console.log("Hello world");
+
+console.log(typeof bigNumber);
+console.log(typeof scoreValue);
+console.log(typeof isLoggedIn);
+console.log(typeof outsideTemp);
+console.log(typeof myFunction);  
+console.log(typeof heros);
+
