11417e65fd0a0df5279a43564e79b4641143f25a
SACH!N <143265500+Sachinsen7@users.noreply.github.com>
2024-04-13 03:00:07 +0000

    string

---
+++ 01_basics/06_string.js
@@ -0,0 +1,14 @@
+const name = "Sachinss"
+const repoCount = 6
+console.log(name + repoCount + "Value");
+
+console.log(` Hello, my name is ${name} and my repo count is ${repoCount}`);
+
+let myName = "Karan"
+
+let anotherName = myName
+anotherName = "shivay"
+
+console.log(myName);
+console.log(anotherName);
+console.log();
