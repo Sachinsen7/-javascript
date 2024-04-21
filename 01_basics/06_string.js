0adf09d4b5c7677a970969f0c43921492923aa95
SACH!N <143265500+Sachinsen7@users.noreply.github.com>
2024-04-21 03:38:04 +0000

    String Methods

--- 01_basics/06_string.js
+++ 01_basics/06_string.js
@@ -1,14 +1,37 @@
 const name = "Sachinss"
 const repoCount = 6
-console.log(name + repoCount + "Value");
+console.log(name + repoCount + "Value");/* outdated*/
 
-console.log(` Hello, my name is ${name} and my repo count is ${repoCount}`);
+console.log(`Hello, my name is ${name} and my repo count is ${repoCount}`);
 
-let myName = "Karan"
+const gameName = new String('Sachin-ss')
 
-let anotherName = myName
-anotherName = "shivay"
+console.log(gameName[0]);
+console.log(gameName.__proto__);
 
-console.log(myName);
-console.log(anotherName);
-console.log();
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('h'));
+
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-5, 4)
+console.log(anotherString);
+
+const newStringOne = "   Sachin   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://Sachin.com/Sachin%20sen"
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('Qaran'));
+console.log(url.includes('Sachin'));
+
+console.log(gameName.split('-'));
+console.log(gameName);
