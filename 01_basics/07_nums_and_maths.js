f2fbde22334a8e6fe4ba0154bf314c7a00591605
SACH!N <143265500+Sachinsen7@users.noreply.github.com>
2024-04-22 13:36:11 +0000

    Numbers  And  Maths

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,38 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 1253.9954
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-US'));
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//+++++++++++++ Maths ++++++++++++++
+
+console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.9));
+// console.log(Math.ceil(4.9));
+// console.log(Math.floor(4.9));
+
+// console.log(Math.min(4, 5, 0, 9));
+// console.log(Math.max(4, 8, 6, 7));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log((Math.random()*10) + 1);
+
+
+
+
+
+ 
