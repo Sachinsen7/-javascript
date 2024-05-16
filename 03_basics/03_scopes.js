06873aa3331c5998174c3f8d758e9525f86874d4
SACH!N <143265500+Sachinsen7@users.noreply.github.com>
2024-05-16 21:40:07 +0530

    global scope, block scope and Expression

--- 03_basics/03_scopes.js
+++ 03_basics/03_scopes.js
@@ -13,6 +13,46 @@ if (true) {
     
 // }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "Sachin"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "Sachin"
+    if (username === "Sachin") {
+        const website = " Youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
+
+// ++++++++++++++++++++++ interesting +++++++++++++++++++++++++++++
+console.log(addOne(5));
+function addOne(num){
+    return num + 1
+}
+
+// addTwo(5)
+// const addTwo = function(num){
+//     return num + 2
+// }
+
+//it will not access before initialization 
