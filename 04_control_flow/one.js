66eb73fa05978b0bc80acbddbad65d83b6ea41c4
SACH!N <143265500+Sachinsen7@users.noreply.github.com>
2024-05-27 20:45:46 +0530

    control flow in js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,51 @@
+// if 
+
+const isUserLoggedIn = true
+const temperature = 41
+
+
+// if (2 == 3){
+//     console.log("executed");
+// } else {
+//     console.log("wrong");
+// }
+
+
+// if (temperature == 30){
+//     console.log("less than 41");
+// } else {
+//     console.log("temperature is greater than 41");
+// }
+
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`)
+
+// <, >, <=, >=, ==, !=, ===, !===
+
+const balance = 1000
+
+if (balance > 500) console.log("test");
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const LoggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==2) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || LoggedInFromEmail || true) {
+    console.log("user logged in");
+}
+
+
+
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,31 @@
+// const  userEmail = "sachi@goggle.com"
+
+// if (userEmail) {
+//     console.log("Got user email");
+// } else {
+//     console.log("Don't have user email");
+// }
+
+const  userEmail =  [] 
+
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
+
+// false values
+// false, 0, -0, BigInt 0n, null, undefined, NaN
+
+// truthy values
+// true, "0", 'false', " ", [], {}, function(){}, 
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+}
