489054f5e3f4210fcd1741570a346da08c8291f8
Sachin Sen <sachinsen1920@gmail.com>
2024-08-23 16:48:05 +0530

    prototype updated

--- 10_oop_and_classes/prototype.js
+++ 10_oop_and_classes/prototype.js
@@ -1,4 +1,4 @@
-let myName = "Sachin"
+// let myName = "Sachin"
 
 // console.log(myName.trim().length);
 // console.log(myName.truelength);
@@ -77,3 +77,67 @@ String.prototype.truelength = function(){
 anotherUsername.truelength()
 "Sachin".truelength()
 "qaran".truelength()
+
+
+/* -----------------------------!----------------------------------------*/
+
+const myNames = ["sachin", "qaran", "ishwar"]
+
+let myName = {
+    firstName: "qaran",
+    secondName: "ishwar",
+    thirdName: "sachin",
+
+    myUniqueness: function(){
+        console.log(`every name has same qualities ${this.firstName}, ${this.secondName}, ${this.thirdName}`);
+        
+    }
+}
+
+console.log(myName.myUniqueness());
+
+Object.prototype.sachin = function(){
+    console.log("sachin is present on all names");
+    
+}
+
+Array.prototype.qaran = function(){
+    console.log("all are sachin");
+    
+}
+
+myName.sachin()
+myNames.qaran()
+
+
+const myLearnings = {
+    first: "python",
+    second: "cpp",
+    third: "javascript"
+}
+
+const teacher = {
+    one: "freecodecamp",
+    two: "hitesh choudhary",
+    three: "code with harry"
+}
+
+const source = {
+    first: "Youtube",
+    second: "mdn",
+    isavailable: true,
+    __proto__: teacher,
+    // __proto__: myLearnings
+}
+
+// source.__proto__ = myLearnings
+
+Object.setPrototypeOf(source, myLearnings)
+
+Object.keys(myLearnings).forEach(key => {
+    console.log(`${key}: ${typeof myLearnings[key]}`);
+  });
+  
+
+
+
