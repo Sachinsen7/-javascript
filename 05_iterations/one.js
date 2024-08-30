5e21e67142bcc4dd80ce9501aa79e9935d2c3ffe
Sachin Sen <sachinsen1920@gmail.com>
2024-08-30 19:43:15 +0530

    added console

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -3,9 +3,10 @@
 for (let i = 0; i <= 10; i++) {
     const element = i;
     if (element == 5) {
-        // console.log("5 is best number");
+        console.log("5 is best number");
     }
-    // console.log(element);
+    console.log(element);
+
 
 }
 
